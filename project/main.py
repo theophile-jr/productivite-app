48247949d26c726e8bc72f88eb40a9ae0d052800
le gac loevan <llegac@jolsio.net@P-17020-G407.jolsio.net>
2021-06-04 08:54:26 +0200

    First try ecole direct

--- project/main.py
+++ project/main.py
@@ -13,3 +13,9 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/ecoledirecte')
+@login_required
+def ecoledirecte():
+    return render_template('ecoledirecte.html')
+
