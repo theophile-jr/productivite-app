52d49a9c620c491913f98df2f3fceb03ff40063c
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-06-04 20:43:35 +0200

    fix setup.py : le path pour le .bat doit utiliser des antislash pour windows

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ print (platform)
 print ("Init Environment with production mode and start server...")
 if platform == "linux" or platform == "darwin":
     subprocess.call("./lib/init/setup.sh")
-else:
-    subprocess.call("./lib/init/setup.bat")
+elif platform == "win32" or platform == "win64":
+    subprocess.call(r".\lib\init\setup.bat")
 print ("done")
