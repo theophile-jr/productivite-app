a143f7a559f9c2c54a20d1208a2c10d523cfe990
Adrien DERACHE <a.d44@tuta.io>
2021-06-05 17:28:56 +0200

    Possibility to mark the task as 'done' (local only)

--- project/main.py
+++ project/main.py
@@ -1,7 +1,11 @@
 # main.py
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, request, jsonify, Flask
 from flask_login import login_required, current_user
+
+import sqlite3
+import json
+
 from .ecoleDirecte import EcoleDirecte as ED
 
 main = Blueprint('main', __name__)
@@ -13,7 +17,57 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name, ecoledirecte=bool(current_user.ed_username))
+    return render_template('profile.html', name=current_user.name)
+
+@main.route('/todo')
+@login_required
+def todo():
+    return render_template('todo.html', name=current_user.name)
+
+# Todo section
+# Add the elements added by the user in the DB
+@main.route('/', methods=['POST'])
+@login_required
+def todo_post():
+#     Get data from the DOM
+    data = request.get_data()
+    data = data.decode("utf-8") # Convert to UTF-8, currently in byte
+    data = json.loads(data) # Convert string to dictionary
+
+    connection = sqlite3.connect('db.sqlite') #Connect to DB
+    cursor = connection.cursor()
+
+#     Add the elements in the DB
+    cursor.execute("INSERT INTO todo (userID, task, date, priority, status) VALUES ('" + current_user.name + "', '" + data['task'] + "', '" + data['date'] + "', '" + data['priority'] + "', 'enable')")
+
+    cursor.execute("SELECT * FROM todo")
+
+    data_list = cursor.fetchall()
+    print(data_list)
+    connection.commit() #Save changes
+    connection.close()
+    return "Success"
+
+@main.route('/getdata')
+@login_required
+def getdata():
+    return render_template('getdata.html')
+
+@main.route('/getdata', methods=['POST'])
+@login_required
+def todo_get():
+    # get all tasks of the current user
+    connection = sqlite3.connect('db.sqlite') #Connect to DB
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM todo WHERE userID='{current_user.name}'")
+    data_list = cursor.fetchall()
+    #print(data_list)
+
+    connection.commit() #Save changes
+    connection.close()
+    print (data_list)
+    return jsonify(data_list)
+
 
 @main.route('/ecoledirecte')
 @login_required
--- project/static/js/todo.js
+++ project/static/js/todo.js
@@ -21,6 +21,9 @@ function updateTask () {
     console.log(taskList[0].name)
     //Create elements
     for (i=0; i < taskList.length; i++) {
+
+        taskNameWithoutSpace = taskList[i].name.replace(/\s/g, '');
+
         //Check if the element already exists
         if (!document.getElementById(taskList[i].name + "Task" + i)) {
             var tr = document.createElement("tr");
@@ -29,8 +32,13 @@ function updateTask () {
 
             //Add the name and the checkbox
             var td = document.createElement("td");
-            td.innerHTML = "<input type=\"checkbox\"> " + taskList[i].name;
+            console.log(taskList[i].status)
+            td.innerHTML = "<input type=\"checkbox\" id=\""+taskNameWithoutSpace+i+"CheckBox"+"\" onchange=\"taskStatusChanged(this.id); sendTodoForm('updateTaskStatus', taskList[i].taskID)\" > " + taskList[i].name;
             document.getElementById(taskList[i].name + "Task" + i).appendChild(td);
+            if (taskList[i].status == "disable") {
+                document.getElementById("" + taskNameWithoutSpace + i + "CheckBox" + "").checked = true;
+                taskStatusChanged ("" + taskNameWithoutSpace + i + "CheckBox" + "")
+            }
 
             //Add the date
             var td = document.createElement("td");
@@ -63,7 +71,6 @@ function sendTodoForm() {
     });
 }
 
-
 window.onload = function GetTodoData() {
     //DESCRIPTION : Send data of to the backend
     //Parameters : None
@@ -78,9 +85,11 @@ window.onload = function GetTodoData() {
             for (y=0; y<todoData.length; y++) {
                 console.log(y)
                 taskList.push({
-                    name: todoData[y][1],
-                    date: todoData[y][2],
-                    priority: todoData[y][3]
+                    taskID: todoData[y][0],
+                    name: todoData[y][2],
+                    date: todoData[y][3],
+                    priority: todoData[y][4],
+                    status : todoData[y][5]
                 });
                 console.log(taskList)
 
@@ -91,6 +100,16 @@ window.onload = function GetTodoData() {
             console.log("Une erreur");
         }
     });
+}
+
+function taskStatusChanged (checkBoxID)
+{
+    console.log(checkBoxID.id);
+    if (document.getElementById(checkBoxID).checked)
+        console.log($("#"+ checkBoxID).parentsUntil("table").addClass("line-through"))
+    else
+        console.log($("#"+ checkBoxID).parentsUntil("table").removeClass("line-through"))
+
 }
 
 // function clearForm(idForm) {
